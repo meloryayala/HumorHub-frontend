7ca9bbd7366f15ed4a3f4c512d47ff06a264a46d
Melory Ayala <melory.ayala@gmail.com>
2023-11-11 13:19:14 +0100

    feat: fetch categories data on categoryList

--- src/components/custom/categoryList.tsx
+++ src/components/custom/categoryList.tsx
@@ -1,11 +1,40 @@
-import {Button} from "@/components/ui/button";
+"use client"
 
-export const CategoryList = ({}) => {
+import {Button} from "@/components/ui/button";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+interface CategoriesData {
+    id: number;
+    name: string;
+}
+
+export const CategoryList = () => {
+    const [categories, setCategories] = useState<CategoriesData[]>([]);
+
+    useEffect(() => {
+       axios.get("https://backend.melory.codery.ch/category")
+           .then(response => {
+               setCategories(response.data)
+           })
+    },[]);
+
+    console.log(categories)
+
+
     return (
         <div className="flex">
             <Button variant="outline">
                 🏷️ Category name
             </Button>
+
+            {
+                categories.map(category => (
+                    <Button key={category.id} variant="outline">
+                        {category.name}
+                    </Button>
+                ))
+            }
         </div>
     );
 };
